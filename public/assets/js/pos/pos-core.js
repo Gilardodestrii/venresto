c1af6b28000b6a4fd0d8837877491152c3c67cb2
gilardodestri <101810207+Gilardodestrii@users.noreply.github.com>
2026-05-27 15:40:50 +0700

    Add POS totals calculator helper

--- public/assets/js/pos/pos-core.js
+++ public/assets/js/pos/pos-core.js
@@ -5,5 +5,39 @@ window.posCore = {
 
     parseNumber(value = '') {
         return parseInt(String(value).replace(/[^0-9]/g, '')) || 0;
+    },
+
+    calculateTotals(cart = [], settings = {}, discount = 0) {
+
+        let subtotal = 0;
+
+        cart.forEach(item => {
+            subtotal += Number(item.qty) * Number(item.price);
+        });
+
+        let baseAmount = Math.max(0, subtotal - discount);
+
+        let service = 0;
+
+        if (settings.service_enabled && !settings.service_inclusive) {
+            service = Math.round(baseAmount * settings.service_rate);
+        }
+
+        let taxBase = baseAmount + service;
+
+        let tax = 0;
+
+        if (settings.tax_enabled && !settings.tax_inclusive) {
+            tax = Math.round(taxBase * settings.tax_rate);
+        }
+
+        let grandTotal = Math.max(0, baseAmount + service + tax);
+
+        return {
+            subtotal,
+            service,
+            tax,
+            grandTotal,
+        };
     }
 };
