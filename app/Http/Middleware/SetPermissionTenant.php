3e81115132431ae8af403d43e32132cd82e6a766
gilardodestri <101810207+Gilardodestrii@users.noreply.github.com>
2026-05-27 09:09:52 +0700

    Set permission tenant team id

---
+++ app/Http/Middleware/SetPermissionTenant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Services\TenantContext;
+use Closure;
+use Illuminate\Http\Request;
+use Spatie\Permission\PermissionRegistrar;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetPermissionTenant
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($tenant = TenantContext::get()) {
+            app(PermissionRegistrar::class)->setPermissionsTeamId($tenant->id);
+        }
+
+        return $next($request);
+    }
+}
